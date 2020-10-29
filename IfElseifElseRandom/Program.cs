dca52a707661025c03e8a361193f3b0a706f513d
recife25 <felipe.j.ruiz64@gmail.com>
2020-10-29 17:25:22 -0400

    added ifElseifRandom and SwitchStatement

---
+++ IfElseifElseRandom/Program.cs
@@ -0,0 +1,45 @@
+﻿//TrueCoders
+//Felipe Ruiz 
+//Fall2020 - selection statements 
+
+using System;
+
+namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the # guessing game");
+            Console.WriteLine("Try to guess the correct number");
+            Console.WriteLine("What is the upper limit for the range of number");
+
+            var userInput = (Console.ReadLine());
+
+            var upperLimit = int.Parse(userInput);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("guess is too high , later");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("guess is too low , later");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+
+        }
+    }
+}
---
+++ switchStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿//Felipe Ruiz 
+//favorite school subject with switch statement
+using System;
+
+namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject at school");
+            var subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is very important");
+                    break;
+                case "Political Science":
+                    Console.WriteLine("Pol Sci is vital to our Democracy");
+                    break;
+                case "Economic":
+                    Console.WriteLine("Economic teachs you about your economy");
+                    break;
+                case "Computer Science":
+                    Console.WriteLine("Need a job - tehc is where is at");
+                    break;
+                default:
+                    Console.WriteLine("ah i haven't taken that subject before");
+                    break;
+
+
+            }
+        }
+    }
+}
